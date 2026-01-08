89b70b5608e09ad175c6e88b51ac2dba1bf45c79
mike <omodingmike@gmail.com>
2026-01-08 10:13:15 +0300

    my pending work

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,15 @@ export interface PageProps {
     searchParams: Promise<SearchParams>;
     params: Promise<{ id: string; }>;
 }
+export interface ServicePackage {
+    id: string
+    name: string
+    description: string
+    formatted_base_price: string
+    formatted_suv_surcharge: string
+    base_price: number
+    suv_surcharge: number
+    features: string[]
+    is_popular: boolean
+    active: boolean
+}
